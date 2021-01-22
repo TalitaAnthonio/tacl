da90b5c240348fdfd87587aae9c6b5340e956713
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-01-22 12:00:02 +0100

    check the revision length of the implicit cases unigrams.

---
+++ wiki-atomic-insertions/check_revision_length.py
@@ -0,0 +1,21 @@
+import json 
+
+path_to_all = '../../../PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/data/wikihow-with-line-numbers.json'
+
+with open(path_to_all, 'r') as json_in:
+    wikihow_all = json.load(json_in)
+
+path_to_other = "../../../PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/data/single_insertions_for_lm_generative_implicit_insertion.json"
+
+with open(path_to_other, "r") as json_in: 
+     single_insertions = json.load(json_in)
+
+
+counter = 0 
+for key in single_insertions.keys(): 
+    if wikihow_all[key]['Revision_Length'] > 1: 
+        print(key) 
+        print(wikihow_all[key]['All_Versions'])
+        counter +=1 
+
+print(counter)
