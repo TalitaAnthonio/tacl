891ba57fd868b3adc6ff25a30013d109087517ef
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-01-25 11:11:21 +0100

    check if there are coreference chains for all the trigram insertions.

--- wiki-atomic-insertions/check_trigrams.py
+++ wiki-atomic-insertions/check_trigrams.py
@@ -1,7 +1,7 @@
 import json 
 
 with open("../data/trigram_atomic_edits.json", 'r') as json_in: 
-        data = json.load(json_in)
+        trigram_atomic_edits = json.load(json_in)
 
 
 path_to_insertions = "/Users/talita/Documents/PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/data/trigrams/benepar_trigram_insertions_needs_coref.json"
@@ -24,22 +24,26 @@ def main():
 
     path_to_trigrams_part1 = "/Users/talita/Documents/PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/data/trigrams/trigrams_coref_part1.json"
     path_to_trigrams_part2 = "/Users/talita/Documents/PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/data/trigrams/trigrams_coref_part2.json"
+    path_to_trigrams_part3 = "../data/trigrams_remaining_atomic_edits_out.json"
 
 
     part1 = read_json_lines(path_to_trigrams_part1)
     part2 = read_json_lines(path_to_trigrams_part2)
+    part3 = read_json_lines(path_to_trigrams_part3) 
     part1.update(part2)
+    part1.update(part3)
 
     counter = 0 
     needs_coref = {}
-    for key, _ in data.items(): 
+    for key, _ in trigram_atomic_edits.items(): 
         if key in part1.keys(): 
            needs_coref[key] = to_be_parsed[key]
-           needs_coref[key].update({"parsed_revised_sentence": part1[key]['parsed_revised_sentence'], "coref": part1[key]['coref'], "insertion_indexes": data[key]["indexes"], "sents": part1[key]['sents']})
+           counter +=1 
+           needs_coref[key].update({"parsed_revised_sentence": part1[key]['parsed_revised_sentence'], "coref": part1[key]['coref'], "insertion_indexes": trigram_atomic_edits[key]["indexes"], "sents": part1[key]['sents']})
 
     print(counter)
-    print(len(data.keys()))
+    print(len(trigram_atomic_edits.keys()))
 
-    with open("../data/trigrams_current_set.json", "w") as json_out:
-         json.dump(needs_coref, json_out)
+    #with open("../data/trigrams_current_set.json", "w") as json_out:
+    #     json.dump(needs_coref, json_out)
 main() 
