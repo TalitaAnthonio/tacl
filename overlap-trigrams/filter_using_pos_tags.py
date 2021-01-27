7d20242840f27cd34a75452977b2392b61dae7aa
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-01-27 15:28:13 +0100

    make trigram_atomic_edits_implicit_pos_filtered.json

--- overlap-trigrams/filter_using_pos_tags.py
+++ overlap-trigrams/filter_using_pos_tags.py
@@ -13,7 +13,6 @@ class RevisedSentence:
         self.revised_sentence = revised_sentence
         self.insertion_index = insertion_index
 
-
     @property 
     def sent_tagged(self): 
         parsed = []
@@ -25,39 +24,39 @@ class RevisedSentence:
     @property 
     def insertion_tagged(self): 
         return self.sent_tagged[self.insertion_index:self.insertion_index+3]
-
-
-def tag_text(revised_sentence): 
-    parsed = []
-    for doc in nlp.pipe([revised_sentence]):
-        for token in doc:
-            parsed.append([token, token.pos_])
-    return parsed
     
-    
-
-def tag_insertion(revised_sentence_tokenized, index): 
-    parsed = []
-    for doc in nlp.pipe([revised_sentence_tokenized]):
-        for token in doc:
-            parsed.append([token, token.pos_])
-    return parsed[index:index+3]
-
-
 
 with open(path_to_file, "r") as json_in: 
      data = json.load(json_in)
 
 print(len(data.keys()))
-for key, _ in data.items(): 
-    revised_sent = data[key]['revised_tokenized']
-    insertion_index = data[key]['insertion_indexes'][0][0]
-    revised_sent_tagged = tag_text(revised_sent)
-    insertion_tagged = tag_insertion(revised_sent, insertion_index)
-    print('----------------------------------------')
 
-    sent = RevisedSentence(revised_sent, insertion_index)
-    res1 = sent.insertion_tagged
-    res2 = insertion_tagged
-    print(res1)
-    print(res2)
+counter = 0 
+filtered_set = {}
+for key, _ in data.items(): 
+    revised_tokenized = data[key]['revised_tokenized']
+    insertion_index = data[key]['insertion_indexes'][0][0]
+
+    revised_sentence = RevisedSentence(revised_tokenized, insertion_index)
+    insertion_tagged = revised_sentence.insertion_tagged
+    pos_tags_in_insertion = [pair[1] for pair in insertion_tagged]
+
+    if 'VERB' not in pos_tags_in_insertion:
+        if 'AUX' not in pos_tags_in_insertion:  
+            if 'CCONJ' not in pos_tags_in_insertion: 
+                counter +=1 
+                print(data[key]['base_tokenized'])
+                print(revised_tokenized)
+                print(insertion_tagged)
+                print(data[key]['insertion_phrases'])
+                print(data[key]['reference']) 
+                filtered_set[key] = data[key]
+    print('--------------------------------------')
+    
+
+print(counter) #2615 
+print(len(data.keys()))
+print(len(filtered_set.keys()))
+
+with open("trigram_atomic_edits_implicit_pos_filtered.json", "w") as json_out: 
+     json.dump(filtered_set, json_out)
