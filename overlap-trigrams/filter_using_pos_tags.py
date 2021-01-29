5cdfde9644fb91086b2d87db2fea5f25017449d6
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-01-29 21:51:06 +0100

    convert the first step into a function.

--- overlap-trigrams/filter_using_pos_tags.py
+++ overlap-trigrams/filter_using_pos_tags.py
@@ -7,6 +7,10 @@ nlp = spacy.load("en_core_web_sm")
 nlp.tokenizer = nlp.tokenizer.tokens_from_list
 
 
+with open(path_to_file, "r") as json_in: 
+     data = json.load(json_in)
+
+
 class RevisedSentence: 
 
     def __init__(self, revised_sentence, insertion_index): 
@@ -24,39 +28,39 @@ class RevisedSentence:
     @property 
     def insertion_tagged(self): 
         return self.sent_tagged[self.insertion_index:self.insertion_index+3]
-    
 
-with open(path_to_file, "r") as json_in: 
-     data = json.load(json_in)
 
-print(len(data.keys()))
 
-counter = 0 
-filtered_set = {}
-for key, _ in data.items(): 
-    revised_tokenized = data[key]['revised_tokenized']
-    insertion_index = data[key]['insertion_indexes'][0][0]
+def first_step_filtering(data):
+    """
+        Remove the insertions from the data that have a VERB, AUX or a CONJUNCTION 
+    """ 
+    filtered_set = {}
+    for key, _ in data.items(): 
+        revised_tokenized = data[key]['revised_tokenized']
+        insertion_index = data[key]['insertion_indexes'][0][0]
 
-    revised_sentence = RevisedSentence(revised_tokenized, insertion_index)
-    insertion_tagged = revised_sentence.insertion_tagged
-    pos_tags_in_insertion = [pair[1] for pair in insertion_tagged]
+        revised_sentence = RevisedSentence(revised_tokenized, insertion_index)
+        insertion_tagged = revised_sentence.insertion_tagged
+        pos_tags_in_insertion = [pair[1] for pair in insertion_tagged]
 
-    if 'VERB' not in pos_tags_in_insertion:
-        if 'AUX' not in pos_tags_in_insertion:  
-            if 'CCONJ' not in pos_tags_in_insertion: 
-                counter +=1 
-                print(data[key]['base_tokenized'])
-                print(revised_tokenized)
-                print(insertion_tagged)
-                print(data[key]['insertion_phrases'])
-                print(data[key]['reference']) 
-                filtered_set[key] = data[key]
-    print('--------------------------------------')
-    
+        if 'VERB' not in pos_tags_in_insertion:
+            if 'AUX' not in pos_tags_in_insertion:  
+                if 'CCONJ' not in pos_tags_in_insertion: 
+                    print(data[key]['base_tokenized'])
+                    print(revised_tokenized)
+                    print(insertion_tagged)
+                    print(data[key]['insertion_phrases'])
+                    print(data[key]['reference']) 
+                    filtered_set[key] = data[key]
+    return filtered_set
 
-print(counter) #2615 
-print(len(data.keys()))
-print(len(filtered_set.keys()))
 
-with open("trigram_atomic_edits_implicit_pos_filtered.json", "w") as json_out: 
-     json.dump(filtered_set, json_out)
+
+def main():
+    print(len(data.keys()))
+
+    filtered_set = first_step_filtering(data)
+    print(len(filtered_set.keys()))
+
+main()
