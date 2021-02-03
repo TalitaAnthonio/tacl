91f37cb0e0f67f319a7397627a28255399975298
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-02-03 10:18:39 +0100

    make fiel implicit_references.json and add split info.

---
+++ all-references/merge_all_implicit_references.py
@@ -0,0 +1,73 @@
+import json 
+from collections import Counter
+
+DEV_FILES_PATH = "/Users/talita/Documents/PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/language-models/dev_files.txt"
+TRAIN_FILES_PATH = "/Users/talita/Documents/PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/language-models/train_files.txt"
+TEST_FILES_PATH = "/Users/talita/Documents/PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/language-models/test_files.txt"
+
+
+def read_file_with_filenames(path_to_file): 
+    with open(path_to_file, "r") as file_in: 
+         content = file_in.readlines()
+         list_of_files = [filename.strip('\n') for filename in content]
+         return list_of_files
+
+DEV_FILES = read_file_with_filenames(DEV_FILES_PATH)
+TEST_FILES = read_file_with_filenames(TEST_FILES_PATH)
+TRAIN_FILES = read_file_with_filenames(TRAIN_FILES_PATH)
+
+
+def check_splits(filename):
+    if filename in DEV_FILES: 
+       return {'Split': 'DEV'}
+    elif filename in TEST_FILES: 
+        return {'Split': 'TEST'} 
+    else: 
+        return {'Split': 'TRAIN'}
+
+def add_split_info(path_to_file): 
+    with open(path_to_file, "r") as json_in: 
+         data = json.load(json_in)
+        
+    
+    data_with_filename_info = {}
+    for key, _ in data.items(): 
+        data_with_filename_info[key] = data[key]
+        split_info = check_splits(data[key]['filename'])
+        data_with_filename_info[key].update(split_info)
+        
+    
+    return data_with_filename_info
+
+
+
+def main(): 
+
+    path_to_bigram_file = "../bigrams/bigram_atomic_edits_implicit_pos_filtered_new.json"
+    path_to_trigram_file = "../overlap-trigrams/trigram_atomic_edits_implicit_pos_filtered.json"
+    path_to_unigram_file = "unigram_edits_implicit_v2.json"
+
+    data = add_split_info(path_to_bigram_file)
+    trigrams = add_split_info(path_to_trigram_file) 
+    unigrams = add_split_info(path_to_unigram_file)
+    
+    data.update(trigrams)
+    data.update(unigrams)
+
+    print(len(data.keys()))
+
+    splits = []
+    for key, _  in data.items(): 
+        splits.append(data[key]['Split'])
+    
+    freq_dict = Counter()
+    for elem in splits: 
+        freq_dict[elem] +=1 
+    print(freq_dict)
+
+
+    with open('implicit_references.json', 'w') as json_out: 
+         json.dump(data, json_out)
+         
+
+main()
