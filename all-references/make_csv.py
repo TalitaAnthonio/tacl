377a9a17e682647c311f2c062a42a340eab58949
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-02-03 15:00:37 +0100

    exclude keys that are spam.

--- all-references/make_csv.py
+++ all-references/make_csv.py
@@ -1,9 +1,7 @@
 import pandas as pd 
 import json 
 
-insertion_type = "unigram"
-
-
+insertion_type = "trigram"
 if insertion_type == "bigram": 
     path_to_file = "../bigrams/bigram_edits_final.json"
     path_to_file_out = "bigram_edits.tsv"
@@ -16,6 +14,18 @@ else:
     path_to_file_out = "unigram_edits.tsv"
 
 
+
+def read_file_with_filenames(path_to_file): 
+    with open(path_to_file, "r") as file_in: 
+         content = file_in.readlines()
+         list_of_files = [filename.strip('\n') for filename in content]
+         return list_of_files
+
+KEYS_TO_DELETE = read_file_with_filenames("keys_to_delete.txt")
+
+
+
+
 with open(path_to_file, "r") as json_in: 
      trigrams = json.load(json_in)
 
@@ -23,19 +33,19 @@ with open(path_to_file, "r") as json_in:
 def main():
     dataframe_dict = {"Base-Sentence": [], "Revised-Sentence": [], "Insertion": [], "reference": [], "reference-type": [], "position-of-ref-in-insertion": [], "par": [], "id": []}
     for key, _ in trigrams.items(): 
-        print(trigrams[key].keys())
-        
-        dataframe_dict["Base-Sentence"].append(trigrams[key]['base_tokenized'])
-        if not insertion_type == 'unigram': 
-            dataframe_dict["Revised-Sentence"].append(trigrams[key]['revised_tokenized'])
-        else: 
-            dataframe_dict["Revised-Sentence"].append(trigrams[key]['revised_sentence'])
-        dataframe_dict["Insertion"].append(trigrams[key]['insertion'])
-        dataframe_dict["id"].append(key)
-        dataframe_dict["reference"].append(trigrams[key]['reference'])
-        dataframe_dict["reference-type"].append(trigrams[key]['reference-type'])
-        dataframe_dict["par"].append(trigrams[key]['par'])
-        dataframe_dict["position-of-ref-in-insertion"].append(trigrams[key]['position-of-ref-in-insertion'])
+        if key not in KEYS_TO_DELETE: 
+            
+            dataframe_dict["Base-Sentence"].append(trigrams[key]['base_tokenized'])
+            if not insertion_type == 'unigram': 
+                dataframe_dict["Revised-Sentence"].append(trigrams[key]['revised_tokenized'])
+            else: 
+                dataframe_dict["Revised-Sentence"].append(trigrams[key]['revised_sentence'])
+            dataframe_dict["Insertion"].append(trigrams[key]['insertion'])
+            dataframe_dict["id"].append(key)
+            dataframe_dict["reference"].append(trigrams[key]['reference'])
+            dataframe_dict["reference-type"].append(trigrams[key]['reference-type'])
+            dataframe_dict["par"].append(trigrams[key]['par'])
+            dataframe_dict["position-of-ref-in-insertion"].append(trigrams[key]['position-of-ref-in-insertion'])
 
 
     df = pd.DataFrame.from_dict(dataframe_dict)
