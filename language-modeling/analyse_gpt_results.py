771195d1b4575f01e61aa1e5239803073fc1a90a
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-02-10 15:15:04 +0100

    finish script to analyse results.

--- language-modeling/analyse_gpt_results.py
+++ language-modeling/analyse_gpt_results.py
@@ -19,14 +19,11 @@ with open(PATH_TO_FILE_IN, 'r') as json_in:
 with open(PATH_TO_FILE_IN_RESULTS, 'r') as json_in: 
      results = json.load(json_in)
 
-def compute_overlap(generated_sequences, correct_insertion, insertion, top_k=100): 
-    "function toc heck if the reference is in the generated sequences"
+def compute_overlap(generated_sequences, correct_insertion, top_k=100): 
+    "function to check if the reference is in the generated sequences"
     generated_sequences_stripped = [sequence.lstrip() for sequence in generated_sequences]
     generated_sequences_stripped = generated_sequences_stripped[:top_k]
-    print('top 10', generated_sequences_stripped)
-    if type(correct_insertion) == list: 
-        correct_insertion = ' '.join(correct_insertion)
-    
+    "Top {0}: {1}".format(top_k, generated_sequences_stripped)
     matches = []
     for generated_sequence in generated_sequences_stripped: 
         if generated_sequence == correct_insertion or generated_sequence.lower() == correct_insertion.lower(): 
@@ -38,85 +35,44 @@ def compute_overlap(generated_sequences, correct_insertion, insertion, top_k=100
     else: 
         return 0 
 
-
-
-#TODO: remove everything with RERANKED 
-#TODO: make sure that the correct insertion is the reference. 
-
-
-def main(): 
-    final_set = {}
+def convert_lines_to_dict_format(path_to_json_lines):
+    results_in_dict_format = {}
     bigram_references.update(single_references)
-    with open(path_to_file) as json_in: 
+    with open(path_to_json_lines) as json_in: 
             for line in json_in: 
                 line = json.loads(line)
                 key = line['key']
-                final_set[key] = line
-                additional_info = {"index_of_reference": bigram_references[key]['index_of_reference'], "index_of_insertion": bigram_references[key]['index_of_insertion'], 'Split': split_info[key]['Split']}
-                final_set[key].update(additional_info)
+                final_set[key] = line 
+                # add data from other file 
+                final_set[key].update(data[key])
+    return results_in_dict_format
 
 
+def main(): 
+
+    results_in_dict_format = convert_lines_to_dict_format(PATH_TO_FILE_IN_RESULTS)
 
     counter = 0 
     total = 0 
-    set_for_perplexity = {}
-    total_insertion = 0 
     bar = Bar('Processing ...', max=len(final_set.keys()))
-    for key, _ in final_set.items(): 
-        if 'insertion' in final_set[key].keys():
-            if final_set[key]['Split'] == 'TEST': 
-                bar.next()
-                #if final_set[key]['Split'] == 'TEST': 
-                index_of_insertion = final_set[key]['index_of_insertion']
-                index_of_reference = final_set[key]['index_of_reference']
-                insertion = final_set[key]['insertion']
-                if index_of_insertion == index_of_reference and type(index_of_insertion[0]) == list: 
-                    correct_insertion = final_set[key]['revised_sentence'][index_of_reference[0][0]:index_of_reference[0][1]+1]
-                    if not RERANK: 
-                        res_with_perplex = final_set[key]['predictions']['generated_texts']
-                    else: 
-                        res_with_perplex = reranked[key]['generated_sequences_perplex']
-                    #res_with_perplex = rerank_using_perplexity_bigrams(final_set[key]['revised_sentence'], final_set[key]['predictions']['generated_texts'],index_of_reference[0][0], index_of_reference[0][1]+1)
-            
-                                        
-                else: 
-                    if len(final_set[key]['insertion']) == 1: 
-                        assert index_of_reference == index_of_insertion
-                        assert len(index_of_insertion) == 1
-                        correct_insertion = final_set[key]['revised_sentence'][index_of_insertion[0]]
-                        if not RERANK: 
-                            res_with_perplex = final_set[key]['predictions']['generated_texts']
-                        else: 
-                            #res_with_perplex = reranked[key]['generated_sequences_perplex']
-                            res_with_perplex = rerank_using_perplexity(final_set[key]['revised_sentence'], final_set[key]['predictions']['generated_texts'], index_of_reference[0])                    
-                    else: 
-                        assert len(index_of_insertion[0]) == 2
-                        correct_insertion = final_set[key]['revised_sentence'][index_of_insertion[0][index_of_reference]]
-                        if not RERANK: 
-                            res_with_perplex = final_set[key]['predictions']['generated_texts']
-                        else: 
-                            #res_with_perplex = reranked[key]['generated_sequences_perplex']
-                            res_with_perplex = rerank_using_perplexity(final_set[key]['revised_sentence'], final_set[key]['predictions']['generated_texts'], index_of_insertion[0][index_of_reference]) 
-                    
-                
-                #correct_or_not = compute_overlap(res_with_perplex, correct_insertion, insertion)
-                #counter += correct_or_not
-                total +=1 
-                if total == 2: 
-                   break 
-                #print(counter)
-                #print(total)
+    for key, _ in results_in_dict_format.items(): 
+        if results_in_dict_format[key]['Split'] == 'TEST': 
+            bar.next()
+            top100_predictions = results_in_dict_format[key]['predictions']['generated_texts']
+            # do the extra step here 
+            correct_reference = results_in_dict_format[key]['reference']
+            if type(correct_reference) != str: 
+               correct_reference = ' '.join(correct_reference)
 
-            set_for_perplexity[key] = final_set[key]
-            set_for_perplexity[key].update({"generated_sequences_perplex": res_with_perplex})
-            set_for_perplexity[key].update({"Split": final_set[key]['Split']})
-    bar.finish()          
-    #print(total)
-    #print(counter)
 
-    #print(len(set_for_perplexity))
-    with open('references_reranked_dev_test_revised_context_finetuned_reranked.json', 'w') as json_out: 
-          json.dump(set_for_perplexity, json_out)
+            # compute 
+            correct_or_not = compute_overlap(generated_sequences, correct_insertion, top_k=100)
+            counter += correct_or_not
+
+    bar.finish()
+    print("total correct", counter)          
+    print("total in data", total)
+    print("percentage", counter/total)
 
 
 main()
