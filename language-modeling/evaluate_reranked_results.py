bf83fc07957b83b11c0495d85fb14ac501675bdb
TalitaAnthonio <talita.anthonio@yahoo.com>
2021-02-13 10:24:21 +0100

    evaluate finetuned results and reranked on the test set.

--- language-modeling/evaluate_reranked_results.py
+++ language-modeling/evaluate_reranked_results.py
@@ -1,6 +1,6 @@
 import json 
 
-PATH_TO_RERANKED_FILE = "results-on-test-set-reranked.json"
+PATH_TO_RERANKED_FILE = "results-on-test-set-finetuned-reranked.json"
 
 with open(PATH_TO_RERANKED_FILE, 'r') as json_in: 
      results_in_dict_format = json.load(json_in)
@@ -8,11 +8,11 @@ with open(PATH_TO_RERANKED_FILE, 'r') as json_in:
 
 def compute_overlap(generated_sequences, correct_insertion, top_k=100): 
     "function to check if the reference is in the generated sequences"
-    generated_sequences_stripped = [sequence.lstrip() for sequence in generated_sequences]
+    generated_sequences_stripped = [sequence.lstrip().lower() for sequence in generated_sequences]
     generated_sequences_stripped = generated_sequences_stripped[:top_k]
 
     #print("Top {0}: {1}".format(top_k, generated_sequences_stripped))
-    if correct_insertion in  generated_sequences_stripped: 
+    if correct_insertion.lower() in  generated_sequences_stripped: 
        return 1 
     else: 
         return 0 
@@ -23,7 +23,6 @@ def main():
     for key, _ in results_in_dict_format.items(): 
         if results_in_dict_format[key]['Split'] == 'TEST': 
             total +=1 
-            old = results_in_dict_format[key]['predictions']['']
             top100_predictions = results_in_dict_format[key]['generated_text_perplexity']
             # do the extra step here 
             correct_reference = results_in_dict_format[key]['reference']
@@ -32,7 +31,7 @@ def main():
 
 
             # compute 
-            correct_or_not = compute_overlap(top100_predictions, correct_reference, top_k=10)
+            correct_or_not = compute_overlap(top100_predictions, correct_reference, top_k=1)
             total_correct += correct_or_not 
     
     print(total_correct)
