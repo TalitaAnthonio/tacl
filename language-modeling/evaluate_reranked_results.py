35eee7a358725b5d5074c69194e2fb88fb4e1e89
Talita <talita.anthonio@yahoo.com>
2021-02-19 11:59:23 +0100

    make correct_cases_dev_finetuned_context.json.

--- language-modeling/evaluate_reranked_results.py
+++ language-modeling/evaluate_reranked_results.py
@@ -1,7 +1,7 @@
 import json 
 
-#PATH_TO_RERANKED_FILE = "results-on-dev-set-reranked-context-finetuned.json"
-PATH_TO_RERANKED_FILE = "results-on-test-set-reranked-context-finetuned.json"
+PATH_TO_RERANKED_FILE = "results-on-dev-set-reranked-context-finetuned.json"
+#PATH_TO_RERANKED_FILE = "results-on-test-set-reranked-context-finetuned.json"
 
 
 with open(PATH_TO_RERANKED_FILE, 'r') as json_in: 
@@ -10,11 +10,11 @@ with open(PATH_TO_RERANKED_FILE, 'r') as json_in:
 
 def compute_overlap(generated_sequences, correct_insertion, top_k=100): 
     "function to check if the reference is in the generated sequences"
-    generated_sequences_stripped = [sequence.lstrip() for sequence in generated_sequences]
+    generated_sequences_stripped = [sequence.lstrip().lower() for sequence in generated_sequences]
     generated_sequences_stripped = generated_sequences_stripped[:top_k]
 
     #print("Top {0}: {1}".format(top_k, generated_sequences_stripped))
-    if correct_insertion in  generated_sequences_stripped: 
+    if correct_insertion.lower() in  generated_sequences_stripped: 
        return 1 
     else: 
         return 0 
@@ -22,8 +22,9 @@ def compute_overlap(generated_sequences, correct_insertion, top_k=100):
 def main():
     total_correct = 0 
     total = 0  
+    correct_cases = {}
     for key, _ in results_in_dict_format.items(): 
-        if results_in_dict_format[key]['Split'] == 'TEST': 
+        if results_in_dict_format[key]['Split'] == 'DEV': 
             total +=1 
             top100_predictions = results_in_dict_format[key]['generated_text_perplexity_context']
             # do the extra step here 
@@ -33,12 +34,18 @@ def main():
 
 
             # compute 
-            correct_or_not = compute_overlap(top100_predictions, correct_reference, top_k=1)
+            correct_or_not = compute_overlap(top100_predictions, correct_reference, top_k=10)
+            if correct_or_not == 1: 
+                correct_cases[key] = results_in_dict_format[key]
+
             total_correct += correct_or_not 
     
     print(total_correct)
     print(total)
     print(total_correct/total)
+
+    with open("correct_cases_dev_finetuned_context.json", "w") as json_out: 
+         json.dump(correct_cases, json_out)
     
 
 
