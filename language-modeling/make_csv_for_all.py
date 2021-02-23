3195901ddb0ab135038e18f0fe51cf0954814584
Talita <talita.anthonio@yahoo.com>
2021-02-23 21:26:56 +0100

    make csv for the data that is already there.

--- language-modeling/make_csv_for_all.py
+++ language-modeling/make_csv_for_all.py
@@ -1,11 +1,11 @@
 import json 
 import pandas as pd 
+import pdb
 
 PATH1 = "GPT+finetuning+P-perplexity.json"
 PATH2 = "GPT+finetuning+S-perplexity.json"
 PATH3 = "GPT+S-perplexity.json"
 PATH4 = "GPTandFinetuning.json"
-
 
 def read_dict(path_to_dict): 
     with open(path_to_dict, 'r') as json_in: 
@@ -15,9 +15,12 @@ def read_dict(path_to_dict):
 
 
 def make_dict_for_df(dict_with_results): 
-    d = {'GPT+FinetuningCorrect': [], 'CorrectReference': [], 'LeftContext': [], 'GPTPred': [], 'GPTCorrect': [], 'key': [], 'GPT+FinetuningPred': [], 'GPT+finetuning+P-perplexityPred': [], 'GPT+finetuning+P-perplexityCorr': [], 'GPT+Finetuning+S-perplexityPred': [], 'GPT+Finetuning+S-perplexityCorr': [], 'GPT+S-perplexityPred': [], 'GPT+S-perplexityCorr': []}
+    # GPT+Finetuning+P-perplexityPred
+    #d = {'GPT+FinetuningCorrect': [], 'CorrectReference': [], 'LeftContext': [], 'GPTPred': [], 'GPTCorrect': [], 'key': [], 'GPT+FinetuningPred': [], 'GPT+Finetuning+P-perplexityPred': [], 'GPT+Finetuning+P-perplexityCorr': [], 'GPT+Finetuning+S-perplexityPred': [], 'GPT+Finetuning+S-perplexityCorr': [], 'GPT+S-perplexityPred': [], 'GPT+S-perplexityCorr': []}
+    d =  {'key': [], 'CorrectReference': [], 'LeftContext': [], 'RevisedSentence': [], 'GPTPred': [], 'GPT+S-perplexityPred': [], 'GPT+FinetuningPred': [], 'GPT+Finetuning+P-perplexityPred': [], 'GPT+Finetuning+S-perplexityPred': []}
+    
     for revision_id, _  in dict_with_results.items(): 
-        for key in dict_with_results[revision_id].keys():
+        for key in d.keys():
             d[key].append(dict_with_results[revision_id][key])
 
     return d 
@@ -36,18 +39,17 @@ def main():
     for key, _ in base_model.items(): 
         updated[key] = base_model[key]
 
-        # update using the other models 
-
+   
         updated[key].update(model1[key])
         updated[key].update(model2[key])
         updated[key].update(model3[key])
 
 
-    print(updated[key].keys())
     df_dict = make_dict_for_df(updated)
 
     
     df = pd.DataFrame.from_dict(df_dict)
+    print(df)
     df.to_csv("test.csv", sep='\t', index=False)
 
 
