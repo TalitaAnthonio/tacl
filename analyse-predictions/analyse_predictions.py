304bb3c0c0bac6d0bdd1089df0e47ad1c16d2ea0
Talita <talita.anthonio@yahoo.com>
2021-04-15 17:04:46 +0200

    check predictions based on how often the word occurs in the text.

--- analyse-predictions/analyse_predictions.py
+++ analyse-predictions/analyse_predictions.py
@@ -1,5 +1,8 @@
 import json 
 import pdb 
+from tools import check_if_filler_occurs, tokenize
+import numpy as np 
+
 
 path_to_file = 'bestmodels_predictions.json'
 
@@ -7,9 +10,45 @@ path_to_file = 'bestmodels_predictions.json'
 with open(path_to_file, 'r') as json_in: 
      data = json.load(json_in)
 
-for key, _ in data.items(): 
-    best_model_pred = [elem.strip() for elem in data[key]['GPT+Finetuning+P-perplexityPred']]
-    second_best_model_pred = [elem.strip() for elem in data[key]['GPT+FinetuningPred']]
-    context = data[key]['LeftContext']
-    pdb.set_trace()
-    break 
+
+def check_pos_of_filler(predictions, correct_filler, topk=10): 
+    if correct_filler.lower() in predictions[:topk]: 
+       return 1 
+    else: 
+        return 0 
+
+
+
+
+def main(): 
+    total_found = 0 
+    total_found_other_ranking = 0 
+    for key, _ in data.items(): 
+        best_model_pred = [elem.strip().lower() for elem in data[key]['GPT+Finetuning+P-perplexityPred']]
+        second_best_model_pred = [elem.strip() for elem in data[key]['GPT+FinetuningPred']]
+        context = data[key]['LeftContext']
+        # check what the current position is 
+        occurs_in_pred = check_pos_of_filler(best_model_pred, data[key]['CorrectReference'])
+        total_found += occurs_in_pred
+
+        best_model_pred = second_best_model_pred
+
+
+        # check if we can increase the position based on saliency 
+        context = tokenize(context, ngrams=data[key]['reference-type'])
+        d = check_if_filler_occurs(context, best_model_pred)
+        sorted_d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+        occurs_or_not = check_pos_of_filler(list(sorted_d.keys()), data[key]['CorrectReference'])
+        total_found_other_ranking += occurs_or_not
+
+        print(list(sorted_d.keys()))
+
+
+        
+
+    
+    print(total_found)
+    print(total_found_other_ranking)
+
+
+main()
---
+++ analyse-predictions/tools.py
@@ -0,0 +1,50 @@
+
+import spacy 
+from collections import Counter 
+
+tokenizer = spacy.load('en_core_web_sm')
+
+def check_if_filler_occurs(context, predictions): 
+    d = Counter()
+ 
+    # make a dict with tokens     
+    tokens_in_context = [token for token in context]
+    for token in tokens_in_context: 
+        d[token] += 1
+    
+    d = dict(d)
+
+
+    # make a dict that indicates how often the filler occurs in the previous context. 
+    dict_with_frequencies_per_prediction = {}
+    for prediction in predictions: 
+        if prediction in d.keys(): 
+           dict_with_frequencies_per_prediction[prediction] = d[prediction]
+        else: 
+            dict_with_frequencies_per_prediction[prediction] = 0 
+           
+    return dict_with_frequencies_per_prediction
+        
+
+
+def tokenize(text_to_tokenize, ngrams='unigram'): 
+    tokenized = [token.text.lower() for token in tokenizer(text_to_tokenize)]
+    if ngrams == 'unigram': 
+       return tokenized 
+    else: 
+        tokenized_by_ngram = []
+        if ngrams == 'bigram': 
+           for i in range(len(tokenized)-1): 
+               bigram = tokenized[i] + ' ' + tokenized[i+1]
+               tokenized_by_ngram.append(bigram)
+        else: 
+            for i in range(len(tokenized)-2): 
+               trigram = tokenized[i] + ' ' + tokenized[i+1] + ' ' + tokenized[i+2]
+               tokenized_by_ngram.append(trigram)
+        return tokenized_by_ngram            
+
+
+
+
+if __name__ == "__main__":
+   main()
