ee00baa7fc47d275509f9a6a6f339d0e0d7c3a46
Talita <talita.anthonio@yahoo.com>
2021-04-15 13:34:36 +0200

    add additional info to csv.

--- analyse-predictions/make_json.py
+++ analyse-predictions/make_json.py
@@ -1,4 +1,5 @@
 import json 
+import pdb
 
 DIR = '../language-modeling/'
 
@@ -16,10 +17,32 @@ model2 = read_dict(PATH2)
 # dict_keys(['GPT+Finetuning+P-perplexityPred', 'GPT+Finetuning+P-perplexityCorr'])
 #dict_keys(['GPT+FinetuningCorrect', 'CorrectReference', 'LeftContext', 'GPTPred', 'GPTCorrect', 'key', 'GPT+FinetuningPred', 'RevisedSentence'])
 
+
+# dict_keys(['coref', 'sents', 'filename', 'base_tokenized', 'id', 'revised_tokenized', 'revised_sentence', 
+# 'insertion_phrases', 'Base_Sentence', 'Base_Nr', 'Revised_Nr', 'parsed_revised_sentence', 'Base_Article', 'Base_Article_Clean', 
+# 'par', 'index_of_insertion', 'index_of_reference', 'bigram', 'stats', 'insertion', 'reference', 'reference-type', 'position-of-ref-in-insertion', 
+# 'Split', 'insertion-type', 'language_model_text', 'revised_afer_insertion', 'revised_untill_insertion'])
+
+PATH_TO_FILE_IN_RESULTS = "../data/references_for_lm.json"
+
+with open(PATH_TO_FILE_IN_RESULTS, 'r') as json_in: 
+     other_info = json.load(json_in)
+
+
+
 merged_dict = {}
 for key, _ in model1.items(): 
     merged_dict[key] = model1[key]
     merged_dict[key].update(model2[key])
+
+    merged_dict[key]['revised_untill_insertion'] = other_info[key]['revised_untill_insertion'] 
+    try: 
+        revised_after_insertion = other_info[key]['revised_afer_insertion']
+    except KeyError: 
+        revised_after_insertion = other_info[key]['revised_after_insertion']
+    
+    merged_dict[key]['revised_after_insertion'] = revised_after_insertion
+
 
 with open('bestmodels_predictions.json', 'w') as json_out: 
      json.dump(merged_dict, json_out)
