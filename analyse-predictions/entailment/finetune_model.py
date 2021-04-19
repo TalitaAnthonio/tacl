cb7df155d1c45c7ae40b77785d11af9bf2ac6939
Talita <talita.anthonio@yahoo.com>
2021-04-19 17:16:48 +0200

    split news data into test, train and val.

--- analyse-predictions/entailment/finetune_model.py
+++ analyse-predictions/entailment/finetune_model.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('news.csv')
 train, val_and_test = train_test_split(df, test_size=0.4)
 val, test = train_test_split(val_and_test, test_size=0.5)
-print(train)
-print(val)
-print(test)
+
+train.to_csv("train_news.csv")
+val.to_csv("val_news.csv")
+test.to_csv("test_news.csv")
