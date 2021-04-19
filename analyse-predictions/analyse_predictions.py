efcd3f27d69ea6bc59e3d1f3f5d549efa54d6a0e
Talita <talita.anthonio@yahoo.com>
2021-04-19 15:50:26 +0200

    add current version.

--- analyse-predictions/analyse_predictions.py
+++ analyse-predictions/analyse_predictions.py
@@ -11,7 +11,6 @@ path_to_file = 'bestmodels_predictions.json'
 with open(path_to_file, 'r') as json_in: 
      data = json.load(json_in)
 
-
 def check_pos_of_filler(predictions, correct_filler, topk=10): 
     if correct_filler.lower() in predictions[:topk]: 
        return 1 
@@ -19,12 +18,10 @@ def check_pos_of_filler(predictions, correct_filler, topk=10):
         return 0 
 
 
-
-
 def main(): 
     total_found = 0 
     total_found_other_ranking = 0 
-    index_errors = 0 
+
     counter = 0 
     total_to_include_total = [] 
     number_of_nouns_total = []
@@ -42,13 +39,23 @@ def main():
 
         context = data[key]['LeftContext']
         context = tokenize(context, ngrams=data[key]['reference-type'])
-        d = check_if_filler_occurs(context, finetuned_only_model_predictions)
-        sorted_d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+
+
+        # dictionary with {"filler": how_often_it_occurs_in_the_context}
+        bow = check_if_filler_occurs(context, finetuned_only_model_predictions)
+        pdb.set_trace()
+
+        # sorted_d: sort the bow 
+        sorted_d = dict(sorted(bow.items(), key=lambda item: item[1], reverse=True))
+        
+        # check if the correct filler is among them. 
         occurs_or_not = check_pos_of_filler(list(sorted_d.keys()), data[key]['CorrectReference'])
         total_found_other_ranking += occurs_or_not
+
+
         
         # do something with  POS TAGS --------------
- 
+        '''
         tagged = add_pos_tagging_to_predictions(finetuned_only_model_predictions)
         # collect the fillers to include in a list 
         fillers_to_include_plus_sentence = []
@@ -60,13 +67,9 @@ def main():
 
             if 'NN' in post_tags_from_filler or 'PRP' in post_tags_from_filler or 'PRP$' in post_tags_from_filler: 
                 number_of_nouns +=1 
-                sent_with_filler = ''.join([ data[key]['revised_untill_insertion'] + ' ' + filler + ' ' + data[key]['revised_after_insertion']])
-                fillers_to_include_plus_sentence.append(sent_with_filler)
-                print(sent_with_filler)
+ 
 
         # add necessary elements for data for coreference 
-        data_for_coreference[key] = data[key]
-        data_for_coreference[key].update({"fillers_for_coref_plus_sent": fillers_to_include_plus_sentence})
 
 
         print("---------------------------------------------")
@@ -85,12 +88,8 @@ def main():
     print(np.mean(number_of_nouns_total))
     print(np.std(number_of_nouns_total))
 
+    '''
 
-
-    print("write to json for coreference parsing ... ")
-
-    with open("data_for_coreference.json", 'w') as json_out: 
-         json.dump(data_for_coreference, json_out)
 
 
 main()
