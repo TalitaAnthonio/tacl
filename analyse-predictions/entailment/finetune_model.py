664af8d44e60bd11747dcccd391471766cb9b4be
Talita <talita.anthonio@yahoo.com>
2021-04-19 17:44:46 +0200

    load dataset and use bucket iterator.

--- analyse-predictions/entailment/finetune_model.py
+++ analyse-predictions/entailment/finetune_model.py
@@ -1,12 +1,38 @@
 import pandas as pd 
 import numpy as np 
 from sklearn.model_selection import train_test_split
+import torch 
+from torchtext.legacy.data import Field, TabularDataset, BucketIterator, Iterator
+import torch.nn as nn
+from transformers import BertTokenizer, BertForSequenceClassification
+
+PATH_TO_TRAIN = 'train_news.csv'
+PATH_TO_VAL = 'val_news.csv'
+PATH_TO_TEST = 'test_news.csv'
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# set model parameters 
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+MAX_SEQ_LEN = 128
+PAD_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
+UNK_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.unk_token)
 
 
-df = pd.read_csv('news.csv')
-train, val_and_test = train_test_split(df, test_size=0.4)
-val, test = train_test_split(val_and_test, test_size=0.5)
+# preprocess data 
+# Fields
 
-train.to_csv("train_news.csv")
-val.to_csv("val_news.csv")
-test.to_csv("test_news.csv")
+label_field = Field(sequential=False, use_vocab=False, batch_first=True, dtype=torch.float)
+text_field = Field(use_vocab=False, tokenize=tokenizer.encode, lower=False, include_lengths=False, batch_first=True,
+                   fix_length=MAX_SEQ_LEN, pad_token=PAD_INDEX, unk_token=UNK_INDEX)
+fields = [('label', label_field), ('title', text_field), ('text', text_field)]
+
+
+train, valid, test = TabularDataset.splits(path='.', train=PATH_TO_TRAIN, validation=PATH_TO_VAL,
+                                           test=PATH_TO_TEST, format='CSV', fields=fields, skip_header=True)
+
+train_iter = BucketIterator(train, batch_size=16, sort_key=lambda x: len(x.text),
+                            device=device, train=True, sort=True, sort_within_batch=True)
+
+
+print(vars(train.examples[0]))
+# Iterators
