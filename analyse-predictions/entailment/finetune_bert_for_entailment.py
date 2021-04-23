6e2dbaf591c7a639173c57e7200f24510310bf53
Talita <talita.anthonio@yahoo.com>
2021-04-23 10:24:39 +0200

    add option for binary classification.

--- analyse-predictions/entailment/finetune_bert_for_entailment.py
+++ analyse-predictions/entailment/finetune_bert_for_entailment.py
@@ -1,65 +1,90 @@
 
 # 3 epochs trainen = 15 minuten ongeveer op de gpu 
 
-from torch.utils.data import DataLoader
-from transformers import DistilBertForSequenceClassification, AdamW, DistilBertTokenizerFast
+from torch.utils.data import DataLoader, TensorDataset
+from transformers import DistilBertForSequenceClassification, AdamW, DistilBertTokenizerFast, BertTokenizer, BertForSequenceClassification
 import torch
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from pathlib import Path
 import pdb
+import pandas as pd 
+from torch.nn.utils.rnn import pad_sequence
 
 NUM_EPOCHS = 3
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+TEST_DF = pd.read_csv("./test.csv")
+TRAIN_DF = pd.read_csv("./train.csv")
+DEV_DF = pd.read_csv("./validation.csv")
+TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=6)
+optimizer = AdamW(model.parameters(), lr=2e-5, correct_bias=False)
 
-def read_imdb_split(split_dir):
+def convert_labels(list_with_labels, binary=False): 
+    list_of_converted_labels = []
+    
+    entails = ['equivalence', 'forward_entailment', 'reverse_entailment']
+
+    if binary: 
+        for label in list_with_labels: 
+            if label == 'equivalence': 
+            list_of_converted_labels.append(0)
+            elif label == 'alternation': 
+                list_of_converted_labels.append(1)
+            elif label == 'other-related': 
+                list_of_converted_labels.append(2)
+            elif label == 'independent': 
+                list_of_converted_labels.append(3)
+            elif label == 'forward_entailment': 
+                list_of_converted_labels.append(4)
+            elif label == 'reverse_entailment': 
+                list_of_converted_labels.append(5)
+            else: 
+                print("there is an errror")
+                pdb.set_trace()
+    
+        return list_of_converted_labels
+    
+    else: 
+        print("using binary .... ")
+        binary_list = [1 if label in entails else 0 for label in list_with_labels]
+        return binary_list
+
+
+
+def read_entailment_data(dataframe):
     """
         Returns a list with texts [" ", " "]
         and a parallel list with labels [1,0,1, ....., ]
     """
-    split_dir = Path(split_dir)
-    texts = []
-    labels = []
-    for label_dir in ["pos", "neg"]:
-        for text_file in (split_dir/label_dir).iterdir():
-            texts.append(text_file.read_text())
-            labels.append(0 if label_dir == "neg" else 1)
-    return texts, labels
+    #'equivalence', 'alternation', 'other-related', 'independent', 'forward_entailment', 'reverse_entailment'}
+    dataframe['context_x'] = dataframe['context_x'].apply(lambda x : x.replace("<x>", "").replace("</x>", ""))
+    dataframe['context_y'] = dataframe['context_y'].apply(lambda x : x.replace("<y>", "").replace("</y>", ""))
+    x_texts = dataframe['context_x'].tolist()
+    y_texts = dataframe['context_y'].tolist()
+    labels = convert_labels(dataframe['semantic_rel'].tolist())
+    return x_texts, y_texts, labels 
 
 
-def compute_accuracy(labels, logits): 
-    """
-        Predicted: tensor with logits [[], [], []] of shape [batch_size, label_size]
-    """
-    accuracy = (logits.max(1).indices  == labels).sum().item() / logits.size(0)
-
-    return accuracy
-
-# dict_keys(['input_ids', 'attention_mask', 'labels']) train_dataset[0]
-class EntailmentDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
-
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
-
-    def __len__(self):
-        return len(self.labels)
-
-
-# -------------- train model ----------------------------------
 def train(model, optim, train_loader, DEVICE, num_epochs=3): 
     epoch_loss = 0 
     epoch_acc = 0 
-    for batch in train_loader:
+    for (pair_token_ids, mask_ids, seg_ids, y) in train_loader:
         optim.zero_grad()
-        input_ids = batch['input_ids'].to(DEVICE)
-        attention_mask = batch['attention_mask'].to(DEVICE)
-        labels = batch['labels'].to(DEVICE)
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+
+        # [16,249]
+        input_ids = pair_token_ids.to(DEVICE)
+        
+        # [16,9] -> deze heeft niet de juiste dimensie
+        attention_mask = mask_ids.to(DEVICE)
+
+        # 16,9
+        seg_ids = seg_ids.to(DEVICE)
+        # 16
+        labels = y.to(DEVICE)
+
+        outputs = model(input_ids, token_type_ids=seg_ids, attention_mask=attention_mask, labels=labels)
+
         loss = outputs.loss
         logits = outputs.logits 
         
@@ -76,7 +101,6 @@ def train(model, optim, train_loader, DEVICE, num_epochs=3):
     
     print("total loss {0} and accuracy {1}".format(epoch_loss/len(train_loader), epoch_acc/len(train_loader)  ))
     return epoch_loss/len(train_loader), epoch_acc / len(train_loader)
-    
 
 def evaluate(model, valid_loader, DEVICE): 
 
@@ -85,11 +109,19 @@ def evaluate(model, valid_loader, DEVICE):
 
     with torch.no_grad(): 
 
-        for batch in valid_loader: 
-            input_ids = batch['input_ids'].to(DEVICE)
-            attention_mask = batch['attention_mask'].to(DEVICE)
-            labels = batch['labels'].to(DEVICE)
-            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        for (pair_token_ids, mask_ids, seg_ids, y) in valid_loader: 
+            input_ids = pair_token_ids.to(DEVICE)
+        
+            # [16,9] -> deze heeft niet de juiste dimensie
+            attention_mask = mask_ids.to(DEVICE)
+
+            # 16,9
+            seg_ids = seg_ids.to(DEVICE)
+             # 16
+            labels = y.to(DEVICE)
+
+            outputs = model(input_ids, token_type_ids=seg_ids, attention_mask=attention_mask, labels=labels)
+           
             loss = outputs.loss
             logits = outputs.logits 
             
@@ -100,34 +132,68 @@ def evaluate(model, valid_loader, DEVICE):
             accuracy_score = compute_accuracy(labels, logits)
             epoch_acc += accuracy_score
             
-    print("total loss {0} and accuracy {1}".format(epoch_loss/len(train_loader), epoch_acc/len(train_loader)  ))
-    return epoch_loss/len(train_loader), epoch_acc / len(train_loader)
+    print("total loss {0} and accuracy {1}".format(epoch_loss/len(valid_loader), epoch_acc/len(valid_loader)  ))
+    return epoch_loss/len(valid_loader), epoch_acc / len(valid_loader)
+
+def compute_accuracy(labels, logits): 
+    """
+        Predicted: tensor with logits [[], [], []] of shape [batch_size, label_size]
+    """
+    accuracy = (logits.max(1).indices  == labels).sum().item() / logits.size(0)
+
+    return accuracy
+
+def encode_data(x_context, y_context, labels): 
+
+    token_ids = []
+    mask_ids = []
+    seg_ids = []
+
+    for (x, y) in zip(x_context, y_context): 
+        x_encoded = TOKENIZER(x, add_special_tokens=False)
+        y_encoded = TOKENIZER(y, add_special_tokens=False)
+
+        #  [CLS] sentence1 [SEP] sentence2 [SEP]
+
+        pair_token_ids = [TOKENIZER.cls_token_id] + x_encoded['input_ids'] + [TOKENIZER.sep_token_id] + y_encoded['input_ids'] + [TOKENIZER.sep_token_id]
+        
+
+        x_length = len(x_encoded['input_ids'])
+        y_length = len(y_encoded['input_ids'])
+        segment_ids = torch.tensor([0] * (x_length + 2) + [1] * (y_length + 1))  # sentence 0 and sentence 1
+        attention_mask_ids = torch.tensor([1] * (x_length + y_length + 3))  # mask padded values
+
+
+        token_ids.append(torch.tensor(pair_token_ids))
+        seg_ids.append(segment_ids)
+        mask_ids.append(attention_mask_ids)
+
+    token_ids = pad_sequence(token_ids, batch_first=True)
+    mask_ids = pad_sequence(mask_ids, batch_first=True)
+    seg_ids = pad_sequence(seg_ids, batch_first=True)
+    labels = torch.tensor(labels)
+    dataset = TensorDataset(token_ids, mask_ids, seg_ids, labels)
+
+    return dataset
+
 
 
 
 def main(): 
+    x_context_train, y_context_train, labels_train = read_entailment_data(TRAIN_DF)
+    x_context_test, y_context_test, labels_test = read_entailment_data(TEST_DF)
+    x_context_dev, y_context_dev, labels_dev = read_entailment_data(DEV_DF)
 
-    model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
-    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    train_encoded = encode_data(x_context_train, y_context_train, labels_train)
+    test_encoded = encode_data(x_context_test, y_context_test, labels_test)
+    dev_encoded =  encode_data(x_context_dev, y_context_dev, labels_dev)
 
-    train_texts, train_labels = read_imdb_split('aclImdb/train')
-    test_texts, test_labels = read_imdb_split('aclImdb/test')
-    train_texts, val_texts, train_labels, val_labels = train_test_split(train_texts, train_labels, test_size=.2)
-
-
-    train_encodings = tokenizer(train_texts, truncation=True, padding=True)
-    val_encodings = tokenizer(val_texts, truncation=True, padding=True)
-    test_encodings = tokenizer(test_texts, truncation=True, padding=True)
-
-    train_dataset = IMDbDataset(train_encodings, train_labels)
-    val_dataset = IMDbDataset(val_encodings, val_labels)
-    test_dataset = IMDbDataset(test_encodings, test_labels)
-
+    test_loader = DataLoader(test_encoded, shuffle=True, batch_size=16)
+    train_loader = DataLoader(train_encoded, shuffle=True, batch_size=16)
+    dev_loader = DataLoader(dev_encoded, shuffle=True, batch_size=16)
 
 
     print("------------training --------------------------------------")
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    valid_loader = DataLoader(val_dataset, batch_size = 16, schuffle=True)
     
     model.to(DEVICE)
     model.train()
@@ -143,7 +209,6 @@ def main():
     model.eval()
     for epoch, _ in enumerate(range(NUM_EPOCHS),1):
          print("------- epoch {0}".format(epoch))
-         evaluate(model, valid_loader, DEVICE)
-
+         evaluate(model, dev_loader, DEVICE)
 
 main()
