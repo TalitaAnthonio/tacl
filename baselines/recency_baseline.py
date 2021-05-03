7882632aa589d73ec59589678e0b46a54ae5da07
Talita <talita.anthonio@yahoo.com>
2021-05-03 12:14:09 +0200

    stage current.

--- baselines/recency_baseline.py
+++ baselines/recency_baseline.py
@@ -31,7 +31,6 @@ with open(coref_chain_info_trigrams_path, 'r') as json_in:
 # add everything together 
 coref_data.update(bigrams)
 coref_data.update(trigrams)
-print(len(coref_data.keys()))
 
 
 remaining_corefs = "/Users/talita/Documents/PhD/corpora/rulebook_diffs/2019-09-23/boardgame_scripts/wikihow/data/unigrams/SingleInsertions_coreferenced.json"
@@ -63,9 +62,9 @@ def get_ref_per_sent(coref_info):
 
             if sentence_index not in reference_index_list.keys(): 
                 reference_index_list[sentence_index] = []
-                reference_index_list[sentence_index].append([reference, begin_index, sent])
+                reference_index_list[sentence_index].append([reference, begin_index])
             else: 
-                reference_index_list[sentence_index].append([reference, begin_index, sent])
+                reference_index_list[sentence_index].append([reference, begin_index])
     return reference_index_list
 
 
@@ -81,19 +80,74 @@ def main():
 
         sentence_indexes = sorted(reference_index_list.keys()) 
         sentence_indexes.reverse()
-        print(sentence_indexes)
-     
+        
+        # just used for printing 
         for sentence_index in sentence_indexes:
-            sorted_references = sorted(reference_index_list[sentence_index], key=lambda x:x[-2], reverse=True) 
+            sorted_references = sorted(reference_index_list[sentence_index], key=lambda x:x[-1], reverse=True) 
+            indexes = [elem[-1] for elem in sorted_references]
 
             print(sentence_index, sorted_references)
-            references_per_sentence = [" ".join(elem[0]) for elem in sorted_references]
-            indexes = [elem[1] for elem in sorted_references]
+        
+        # just used for printing 
+        
+        print(sentence_indexes)
 
-            if data[key]["CorrectReference"] in references_per_sentence: 
-               print(references_per_sentence, "in data", data[key]["CorrectReference"])
-               break 
-           
+        # take the reference and the correct index 
+        if "reference" in data[key].keys():
+            correct_reference = data[key]["reference"]
+        else: 
+            correct_reference = data[key]["CorrectReference"]
+        index_of_reference = data[key]["index_of_reference"]
+        correct_reference_index_pair = [correct_reference.split(), index_of_reference]
+
+        # print all the referring expressions of the revised sentence. 
+        referring_expressions_in_revised_pairs = sorted(reference_index_list[sentence_indexes[0]], key=lambda x:x[-1], reverse=True)
+        try: 
+            position_of_correct_reference = referring_expressions_in_revised_pairs.index(correct_reference_index_pair)
+            print("nr of references in revised", len(referring_expressions_in_revised_pairs))
+            counter +=1 
+
+            # if there is just one refererring expression in the revised sentence (which should be the human-inserted one), take the closest referring expression of the previous sentence. 
+            if len(referring_expressions_in_revised_pairs) == 1: 
+               referring_expressions_in_previous_sentence = sorted(reference_index_list[sentence_indexes[1]], key=lambda x:x[-1], reverse=True)[0][0]
+               print("The nearest reference is", referring_expressions_in_previous_sentence)
+            else: 
+               # check the cataphors with one sentence 
+               if len(sentence_indexes) == 1: 
+                  print("Cataphoric")
+                  print(referring_expressions_in_revised_pairs)
+               
+               else: 
+                    # check the closest referring expression in the previous sentence 
+                    closest_referring_expression_in_the_previous_sentence = sorted(reference_index_list[sentence_indexes[1]], key=lambda x:x[-1], reverse=True)[0]
+                    print("the closest referring expression in the previous sentence", closest_referring_expression_in_the_previous_sentence)
+                    if position_of_correct_reference == 0: 
+                       next_referring_expression_in_revised_sentence = sorted(reference_index_list[sentence_indexes[0]], key=lambda x:x[-1], reverse=True)[1]
+                       absolute_distance_between_ref_in_same = abs(correct_reference_index_pair[-1] - next_referring_expression_in_revised_sentence[-1])
+                       absolute_distance_between_ref_in_prev = abs(correct_reference_index_pair[-1] - closest_referring_expression_in_the_previous_sentence[-1])
+                       print(absolute_distance_between_ref_in_same, "same sent")
+                       print(absolute_distance_between_ref_in_prev, "prev sent")
+
+                       print("closest referring expression", next_referring_expression_in_revised_sentence)
+                       print("correct reference", correct_reference_index_pair)
+
+
+                       if absolute_distance_between_ref_in_same > absolute_distance_between_ref_in_prev: 
+                          print("closest reference", closest_referring_expression_in_the_previous_sentence)
+                       else: 
+                          print("closest reference", next_referring_expression_in_revised_sentence)
+            # de lengte is 1 
+            # de lengte is langer dan 1 
+        except ValueError: 
+            print(referring_expressions_in_revised_pairs)
+            print(correct_reference_index_pair)
+            print(key)
+            print("============to check ==================")
+
+
+            
+
         print("======================")
-        break 
+    
+    print(counter)
 main()
